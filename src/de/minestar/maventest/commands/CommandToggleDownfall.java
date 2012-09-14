943cae3c7dd66c2d8957143b30c58d9bc797a2e7
Meldanor <Bhaals@gmx.de>
2012-09-14 20:54:49 +0200

    Implement toggledownfall command
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/de/minestar/maventest/commands/CommandToggleDownfall.java
@@ -0,0 +1,24 @@
+package de.minestar.maventest.commands;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.src.ICommandSender;
+import de.minestar.maventest.commandsystem.AbstractCommand;
+import de.minestar.maventest.commandsystem.ArgumentList;
+import de.minestar.maventest.commandsystem.MinestarCommandHandler;
+import de.minestar.maventest.commandsystem.annotations.Arguments;
+import de.minestar.maventest.commandsystem.annotations.Description;
+import de.minestar.maventest.commandsystem.annotations.Label;
+
+@Label(label = "toggledownfall")
+@Arguments(arguments = "")
+@Description(description = "This is a normal command. It is a single command with 1 needed parameter and endless optional parameters.")
+public class CommandToggleDownfall extends AbstractCommand {
+
+    @Override
+    public void execute(ICommandSender sender, ArgumentList argumentList) {
+        MinecraftServer.getServer().theWorldServer[0].commandToggleDownfall();
+        MinecraftServer.getServer().theWorldServer[0].getWorldInfo().setThundering(true);
+        MinestarCommandHandler.notifyAdmins(sender, "commands.downfall.success");
+    }
+
+}
