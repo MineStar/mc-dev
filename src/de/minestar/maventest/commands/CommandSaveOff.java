197feff5cd8e2281f237c657c65ee35b4b87466c
Meldanor <Bhaals@gmx.de>
2012-09-14 19:01:09 +0200

    Implement save off command
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/de/minestar/maventest/commands/CommandSaveOff.java
@@ -0,0 +1,31 @@
+package de.minestar.maventest.commands;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.src.ICommandSender;
+import de.minestar.maventest.commandsystem.AbstractCommand;
+import de.minestar.maventest.commandsystem.ArgumentList;
+import de.minestar.maventest.commandsystem.MinestarCommandHandler;
+import de.minestar.maventest.commandsystem.annotations.Arguments;
+import de.minestar.maventest.commandsystem.annotations.Description;
+import de.minestar.maventest.commandsystem.annotations.Label;
+
+@Label(label = "save-off")
+@Arguments(arguments = "")
+@Description(description = "This is a normal command. It is a single command with 1 needed parameter and endless optional parameters.")
+public class CommandSaveOff extends AbstractCommand {
+
+    @Override
+    public void execute(ICommandSender sender, ArgumentList argumentList) {
+
+        MinecraftServer server = MinecraftServer.getServer();
+
+        for (int i = 0; i < server.theWorldServer.length; ++i) {
+            if (server.theWorldServer[i] != null) {
+                server.theWorldServer[i].levelSaving = true;
+            }
+        }
+
+        MinestarCommandHandler.notifyAdmins(sender, "commands.save.disabled");
+    }
+
+}
