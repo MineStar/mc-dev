995a2de407acb0fda1ba766e8a881fbc28f5e98e
GeMoschen <x-chris-x@arcor.de>
2012-09-16 12:41:46 +0200

    - reworked /time to use keywords

--- src/de/minestar/commands/time/CommandTime.java
+++ src/de/minestar/commands/time/CommandTime.java
@@ -1,25 +1,53 @@
 package de.minestar.commands.time;
 
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.ICommandSender;
+import net.minecraft.src.WorldServer;
 import de.minestar.commandsystem.AbstractCommand;
 import de.minestar.commandsystem.ArgumentList;
+import de.minestar.commandsystem.CommandHandler;
+import de.minestar.commandsystem.ParseUtils;
 import de.minestar.commandsystem.annotations.Arguments;
 import de.minestar.commandsystem.annotations.Label;
 
 @Label(label = "time")
-@Arguments(arguments = "")
+@Arguments(arguments = "SET|ADD <Time>")
 public class CommandTime extends AbstractCommand {
 
     @Override
-    protected void createSubCommands() {
-        this.registerCommand(new CommandTimeAdd());
-        this.registerCommand(new CommandTimeSet());
+    public void execute(ICommandSender sender, ArgumentList argumentList) {
+        if (argumentList.getString(0).equalsIgnoreCase("ADD")) {
+            // ADD THE TIME
+            int time = ParseUtils.parseIntWithMin(sender, argumentList.getString(0), 0);
+            this.addTimeInAllWorlds(time);
+            CommandHandler.notifyAdmins(sender, "commands.time.added", time);
+        } else {
+            // SET THE TIME
+            int time = 0;
+            String timeString = argumentList.getString(0).toLowerCase();
+            if (timeString.equalsIgnoreCase("day"))
+                time = 0;
+            else if (timeString.equalsIgnoreCase("night"))
+                time = 12500;
+            else
+                time = ParseUtils.parseIntWithMin(sender, timeString, 0);
+
+            this.setTimeInAllWorlds(time);
+            CommandHandler.notifyAdmins(sender, "commands.time.set", time);
+        }
     }
 
-    @Override
-    public void execute(ICommandSender sender, ArgumentList argumentList) {
-        // DO NOTHING - SUPER COMMAND
+    protected void addTimeInAllWorlds(int time) {
+        for (int i = 0; i < MinecraftServer.getServer().theWorldServer.length; ++i) {
+            WorldServer worldServer = MinecraftServer.getServer().theWorldServer[i];
+            worldServer.setTime(worldServer.getWorldTime() + (long) time);
+        }
+    }
 
+    protected void setTimeInAllWorlds(int time) {
+        for (int index = 0; index < MinecraftServer.getServer().theWorldServer.length; ++index) {
+            MinecraftServer.getServer().theWorldServer[index].setTime((long) time);
+        }
     }
 
 }
